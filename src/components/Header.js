86c8bfb84d1411e50adab33617c42288e44f9a6c
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-08-04 16:40:18 -0400

    added router and storeicper router and not found

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className='top'>
-        <h1> Deep Sea Eatery</h1>
-        <h3 className='tagline'>
-          <span>Fresh Seafood Daily </span>
-        </h3>
-      </header>
-    );
-  }
-}
+const Header = (props) => {
+  return (
+    <header className='top'>
+      <h1> Deep Sea Eatery</h1>
+      <h3 className='tagline'>
+        <span>{props.tagline}</span>
+      </h3>
+    </header>
+  );
+};
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className='top'>
+//         <h1> Deep Sea Eatery</h1>
+//         <h3 className='tagline'>
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
