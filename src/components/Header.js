ec5fbadd9b6456c4124f92d98d950e738132fa24
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-08-04 15:44:09 -0400

    added new components, header, inventory, order

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='top'>
+        <h1> Deep Sea Eatery</h1>
+        <h3 className='tagline'>
+          <span>Fresh Seafood Daily </span>
+        </h3>
+      </header>
+    );
+  }
+}
+
+export default Header;
