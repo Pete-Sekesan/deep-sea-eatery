4b5d9c94500e039b38fd9f9ed0a705e2a279319a
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-08-05 11:35:26 -0400

    saveing valuer and push route from storepicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    //stop form from submittinbg
+    event.preventDefault();
+
+    //get text from input
+    //console.log(this.myInput.value.value);
+    const storeName = this.myInput.current.value;
+    //change page from store to what entered
+    this.props.history.push(`./store/${storeName}`);
+  };
+
   render() {
     return (
       <React.Fragment>
         <p>Fish!!</p>
-        <form className='store-selector'>
+        <form className='store-selector' onSubmit={this.goToStore}>
           <h2>Please Enter A Store</h2>
-          <input type='text' required placeholder='Store Name' />
+
+          <input
+            type='text'
+            ref={this.myInput}
+            required
+            placeholder='Store Name'
+            defaultValue={getFunName()}
+          />
           <button type='submit'>Visit Store </button>
         </form>
       </React.Fragment>
